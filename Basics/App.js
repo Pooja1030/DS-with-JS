b8d849407dfebf597b85f123dfd8c315ae0e757d
Pooja <poojasawant1030@gmail.com>
2025-03-04 19:06:25 +0530

    started with loops

--- Basics/App.js
+++ Basics/App.js
@@ -393,3 +393,129 @@ switch(numm){
     default : console.log("NAN");
     
 }
+
+
+// Loop  -> whatsapp, instagram, facebook, ecommerce, yt
+// for while do-while
+
+// for
+// for(start; end; change){}
+
+for(let i = 200; i > 99; i--){
+    console.log(i);
+}
+
+
+//  sum of n natural numbers
+
+let pr = prompt("Kaha tak add karwaaoge ?");
+ 
+if(pr === null){
+    console.log("cancelled");
+}
+else{
+    let nuum = Number();
+
+if(isNaN(nuum)){
+    console.log("Inalid Input");
+}
+else{
+    if(nuum > 0){
+        let fact = 1;
+        for(let i = 1; i <= numm; i++){
+            fact = fact + i;
+        }
+        console.log(fact);    
+    }
+    else{
+        console.log("Should e +e and more than -");   
+    }
+}
+}
+
+// factorial
+let nuum1 = Number(prompt("Kaha tak add karwaaoge ?"))
+
+if(isNaN(nuum1)){
+    console.log("Inalid Input");
+}
+else{
+    if(nuum1 > 0){
+        let sum = 0;
+        for(let i = 1; i <= numm1; i++){
+            sum = sum + i;
+        }
+        console.log(sum);    
+    }
+    else{
+        console.log("Should e +e and more than -");   
+    }
+}
+
+
+// factors
+let nuum2 = Number(prompt("Kaha tak add karwaaoge ?"))
+
+if(isNaN(numm2)){
+    console.log("Inalid Input");
+}
+else{
+    if(nuum2 > 0){    
+        for(let i = 1; i <= Math.floor(nuum2/2); i++){
+            if(nuum2 % i === 0){
+                console.log(i);
+            }
+        }
+        console.log(nuum2);    
+    }
+    else{
+        console.log("Should e +e and more than -");   
+    }
+}
+
+
+// Prime no
+//  2 is the only een prime no
+
+
+let nuum3 = Number(prompt("Kaha tak add karwaaoge ?"))
+
+if(isNaN(numm3)){
+    console.log("Inalid Input");
+}
+else{
+    if(nuum3 > 0){    
+        let prime = true;
+        for(let i = 2; i <= Math.floor(nuum3/2); i++){
+            if(nuum3 % i === 0){
+                // prime = false;
+                // break;
+                console.log(isPrime(nuum3));
+            }
+        }   
+    }
+    else{
+        console.log("Should e +e and more than -");   
+    }
+}
+
+
+function isPrime(nuum3){ //12
+    if(nuum3 <= 1) return false;
+    if(nuum3 == 2) return true;
+    if(nuum3%2 == 0) return false;
+    for(let i = 3; i <= Math.floor(Math.sqrt(nuum3)); i+=2);
+}
+
+// 37 = 6 = 2->6         -- if there is a no which is not diied y 2 till its sq root then it will not get diided after sq.rt
+
+
+// break and continue
+
+
+// while
+
+while(end){
+    // code
+    change;
+}
