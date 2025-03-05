33b13eaf1681737d5a06f4f475b214e3e7e4bfc6
Pooja <poojasawant1030@gmail.com>
2025-03-05 17:04:17 +0530

    implemented some loops questions

---
+++ Basics/Loops.js
@@ -0,0 +1,257 @@
+
+// Loop  -> whatsapp, instagram, facebook, ecommerce, yt
+// for while do-while
+
+// for
+// for(start; end; change){}
+
+for(let i = 200; i > 99; i--){
+    console.log(i);
+}
+
+
+//  sum of n natural numbers
+
+let pr = prompt("Kaha tak add karwaaoge ?");
+ 
+if(pr === null){
+    console.log("cancelled");
+}
+else{
+    let nuum = Number();
+
+if(isNaN(nuum)){
+    console.log("Inalid Input");
+}
+else{
+    if(nuum > 0){
+        let fact = 1;
+        for(let i = 1; i <= numm; i++){
+            fact = fact + i;
+        }
+        console.log(fact);    
+    }
+    else{
+        console.log("Should e +e and more than -");   
+    }
+}
+}
+
+// factorial
+let nuum1 = Number(prompt("Kaha tak add karwaaoge ?"))
+
+if(isNaN(nuum1)){
+    console.log("Inalid Input");
+}
+else{
+    if(nuum1 > 0){
+        let sum = 0;
+        for(let i = 1; i <= numm1; i++){
+            sum = sum + i;
+        }
+        console.log(sum);    
+    }
+    else{
+        console.log("Should e +e and more than -");   
+    }
+}
+
+
+// factors
+let nuum2 = Number(prompt("Kaha tak add karwaaoge ?"))
+
+if(isNaN(numm2)){
+    console.log("Inalid Input");
+}
+else{
+    if(nuum2 > 0){    
+        for(let i = 1; i <= Math.floor(nuum2/2); i++){
+            if(nuum2 % i === 0){
+                console.log(i);
+            }
+        }
+        console.log(nuum2);    
+    }
+    else{
+        console.log("Should e +e and more than -");   
+    }
+}
+
+
+// Prime no
+//  2 is the only een prime no
+
+
+let nuum3 = Number(prompt("Kaha tak add karwaaoge ?"))
+
+if(isNaN(numm3)){
+    console.log("Inalid Input");
+}
+else{
+    if(nuum3 > 0){    
+        let prime = true;
+        for(let i = 2; i <= Math.floor(nuum3/2); i++){
+            if(nuum3 % i === 0){
+                // prime = false;
+                // break;
+                console.log(isPrime(nuum3));
+            }
+        }   
+    }
+    else{
+        console.log("Should e +e and more than -");   
+    }
+}
+
+
+function isPrime(nuum3){ //12
+    if(nuum3 <= 1) return false;
+    if(nuum3 == 2) return true;
+    if(nuum3%2 == 0) return false;
+    for(let i = 3; i <= Math.floor(Math.sqrt(nuum3)); i+=2);
+}
+
+// 37 = 6 = 2->6         -- if there is a no which is not diied y 2 till its sq root then it will not get diided after sq.rt
+
+
+// break and continue
+
+
+// while
+
+// start;
+// while(end){
+//     // code
+//     change;
+// }
+// when you know there is only condition when you know when to stop we use while
+
+let anss = prompt("kuch hi dedo(exit for close)");
+
+while(anss !== 'exit'){
+    anss = prompt("kuchh hi dedo(exit for close)");
+}
+
+
+// sum of digits
+
+let num1 = Number(prompt("Kaha tak add karwaaoge ?"))
+
+if(isNaN(num1)){
+    console.log("Inalid Input");
+}
+else{
+    if(num1 > 0){ 
+        let sum = 0;   
+        while(num1 > 0){
+        let rem = num1%10;
+        sum = sum + rem;
+        num1 = Math.floor(num1/10);
+       }
+       console.log((sum));
+       
+    }
+    else{
+        console.log("Should e +e and more than -");   
+    }
+}
+
+
+// reerse of a no
+let num2 = Number(prompt("Kaha tak add karwaaoge ?"))
+
+if(isNaN(num2)){
+    console.log("Inalid Input");
+}
+else{
+    if(num2 > 0){ 
+        let re = 0;   
+        while(num2 > 0){
+        let rem = num2%10;
+        re = re*10 + rem;
+        num2 = Math.floor(num2/10);
+       }
+       console.log(re);
+       
+    }
+    else{
+        console.log("Should e +e and more than -");   
+    }
+}
+
+
+// strong no
+
+let num3 = Number(prompt("Kaha tak add karwaaoge ?"))
+
+if(isNaN(num3)){
+    console.log("Inalid Input");
+}
+else{
+    if(num3 > 0){ 
+        let sum = 0;   
+        let copy = num3;
+        while(num3 > 0){
+        let rem = num3%10;
+        let fact = 1;
+        for(let i = 1; i <= rem; i++){
+            fact = fact * i;
+        }
+        sum = sum + fact;
+        num3 = Math.floor(num3/10);
+       }
+       if(copy === sum){
+           console.log("strong");
+       }
+       else{
+        console.log("not strong");
+       }
+       
+    }
+    else{
+        console.log("Should e +e and more than -");   
+    }
+}
+
+
+// do-while
+
+// do{
+//     console.log("hye");
+// }
+// while(12>13)
+
+
+let ii = 10;
+do{
+    console.log("hey");
+    ii++;
+}
+while(i<=10);
+
+// repeat hello
+
+
+// Guess the no
+let random = Math.floor(Math.random()*100) + 1;
+
+let guess = 0;
+while(guess !== random){
+    guess = Number(prompt("Guest the no"))
+    if(isNaN(guess) || guess<1 || guess>100){
+        console.log("Try again etween 1-100");
+        continue;
+    }
+    if(guess>random){
+        console.log("Too high, Try again");
+    } else if(guess < random) {
+        console.log("Too low, Try again");
+    } else{
+        console.log("Congrats!! and no was" , guess);    
+    }
+}
+
+
+
+// sasta calculator
+
---
+++ Basics/conditions.js
@@ -0,0 +1,211 @@
+// Q.8
+
+let ans = Number(prompt("What is your age ?"));
+
+// In JS NaN === NaN
+// isNaN(NaN)
+
+if(isNaN(ans)){
+    console.log("Wrong Input");   
+}
+else if(ans >= 18){
+    console.log("You can ote");
+} else {
+    console.log("You cannot ote");
+}
+
+
+// Q.9
+
+// Range Question
+// brute approach
+let amount = Number(prompt("What is the final amount to be paid?"))
+if(amount > 0 && amount <= 5000){
+    console.log(amount);
+}
+else if(amount > 5000 && amount <= 7000){
+    console.log(amount - Math.floor((5*amount)/100));
+}
+else if(amount > 7000 && amount <= 9000){
+    console.log(amount - Math.floor((10*amount)/100));
+}
+else if(amount > 9000){
+    console.log(amount - Math.floor((20*amount)/100));
+}
+else {
+    console.log("Wrong Input");
+}
+
+// efficient approach
+let amt = Number(prompt("What is the final amount to be paid?"))
+let dis = 0
+if(amt > 0 && amt <= 5000){
+    dis = 0;
+}
+else if(amt > 5000 && amt <= 7000){
+    dis = 5;
+}
+else if(amt > 7000 && amt <= 9000){
+    dis=10;
+}
+else if(amt > 9000){
+    dis = 20
+}
+    console.log(amt - Math.floor((dis*amt)/100));
+
+
+// Q.10
+// bijli 
+// bottom to top approach
+
+let unit = Number(prompt("Enter electricity unit"))   // 700
+let ammount = 0;
+if(unit > 400){
+    ammount = (unit - 400) * 13; //3900
+    unit = 400;
+} //400
+if(unit > 200 && unit <= 400){
+    ammount += (unit - 200) * 8;  //1600
+    unit = 200;
+} //200
+if(unit > 100 && unit <= 200){
+    ammount += (unit - 100) * 6;  //600
+    unit = 100;
+} //
+ammount += unit * 4;
+console.log(ammount);
+
+
+// Q.11
+
+// 4888  - 500 - 9
+// 388  - 200 - 1
+// 188  - 100 - 1
+// 88  - 50 - 1
+// 38
+
+// 4823 / 500 = 9   // kiti notes milenge
+// 4823 % 500 = 323/200 = 1   // kitine bachenge
+
+
+let amnt = 4823;
+if(amnt >= 500){
+    console.log("500 Notes : " + Math.floor(amnt/500));
+    amnt = amnt % 500;
+}
+if(amnt >= 200){
+    console.log("200 Notes : " + Math.floor(amnt/200));
+    amnt = amnt % 200;
+}
+if(amnt >= 100){
+    console.log("100 Notes : " + Math.floor(amnt/100));
+    amnt = amnt % 100;
+}
+if(amnt >= 50){
+    console.log("50 Notes : " + Math.floor(amnt/50));
+    amnt = amnt % 50;
+}
+if(amnt >= 20){
+    console.log("20 Notes : " + Math.floor(amnt/20));
+    amnt = amnt % 20;
+}
+if(amnt >= 10){
+    console.log("10 Notes : " + Math.floor(amnt/10));
+    amnt = amnt % 10;
+}
+if(amnt >= 5){
+    console.log("5 Notes : " + Math.floor(amnt/5));
+    amnt = amnt % 5;
+}
+if(amnt >= 2){
+    console.log("2 Notes : " + Math.floor(amnt/2));
+    amnt = amnt % 2;
+}
+if(amnt === 1){
+    console.log("1 Notes : " + amnt);
+}
+
+
+// Ternary operator ? :
+112 > 14 ? console.log("heheheh") : console.log("huuuuu");
+
+console.log(122>13?"hello":"hey");
+
+// nested ternary operator
+let num = 0;
+console.log(num > 0 ? "positie" : num < 0 ? "Negatie" : "zero");
+
+// switch case
+// fall through condition -- where break is not used and it displays all the outputs
+// when to use -- constant alues are needed to compare , exact alues are needed to get matched
+// There are some things that if else can do which switch can't do but all the switch things they do if else can do
+
+let day = 2;
+switch(day){
+    case 1:  console.log("MOnday");
+    break;
+    
+    case 2:  console.log("Tuesday");
+    break;
+    
+    default:  console.log("NAN");
+}
+
+let name = "harsh";
+switch(day){
+    case "raj":  console.log("MOnday");
+    break;
+    
+    case "harsh":  console.log("Tuesday");
+    break;
+    
+    default:  console.log("NAN");
+}
+
+//   /// /////////////////////
+let days = 5;
+switch(day){
+    case 1:  
+    case 2:
+    case 3:
+      console.log("Tuesday");
+      break;
+    
+    case 4:  
+    case 5:
+    case 6:
+      console.log("Wednesday");
+      break;
+    
+    default:  console.log("NAN");
+}
+
+// ///////////////////////////////////////
+switch(true){
+    case 19 > 6:
+        console.log("Hello");
+        break;
+        
+        
+    case 10 > 9:
+        console.log("Heuuuuu");
+        break;
+             
+    default:  console.log("NAN");
+}
+
+// Precision isssue   
+// how to fix this code 
+////////////////////////////////
+let numm = 0.1 + 0.2
+switch(numm){
+    case 0.3 : console.log("heooo");
+    break;
+    
+    case 0.5 : console.log("heee");
+    break;
+    
+    default : console.log("NAN");
+    
+}
+
