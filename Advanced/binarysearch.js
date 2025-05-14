113ffb4522f90dd3e29b2a651a5378b9d61d077e
Pooja <poojasawant1030@gmail.com>
2025-05-14 15:45:23 +0530

    implemented binary search in rotated array

--- Advanced/binarysearch.js
+++ Advanced/binarysearch.js
@@ -14,3 +14,21 @@ var searchInsert = function(nums, target){
 }
 
 
+
+// search in rotated array
+
+var search = function(nums, target){
+    let first = 0, last = nums.length - 1;
+    while(first <= last){
+        let mid = Math.floor((first + last) / 2);
+        if(nums[mid] == target) return mid;                     // check if mid == target
+        if(nums[first] <= nums[mid]){                           // if first half is smaller than mid then the first half is sorted
+            if(target >= nums[first] && target <= nums[mid]) last = mid;
+            else first = mid + 1;
+        } else {
+            if(target >= nums[mid + 1] && target <= nums[last]) first = mid + 1;
+            else last = mid;
+        }
+    }
+    return -1;
+}
