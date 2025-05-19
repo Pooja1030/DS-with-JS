1e95b430c97a002e25f18118dcab2d92d20726db
Pooja <poojasawant1030@gmail.com>
2025-05-19 15:08:57 +0530

    implemented Book Allocation question

---
+++ Advanced/AdvancedSearch.js
@@ -0,0 +1,41 @@
+// Book Allocation question
+
+function isvalid(arr, mid, k){
+    let noOfStudent = 1;
+    let sum = 0;
+    for(let i = 0; i < arr.length(); i++){
+        if(arr[i] + sum > mid){
+            noOfStudent++;
+            sum = arr[i];
+            if(noOfStudent > k) return false;
+        } else sum += arr[i];
+    }
+    return true;
+}
+
+class Solution {
+    findPages(arr, k){
+
+        if(k > arr.length) return -1;
+
+        let first = 0;
+        let last = 0;
+        let ans = -1;
+        for(let i = 0; i < arr.length; i++){
+            first = Math.max(arr[i], first);
+            last += arr[i];
+        }
+
+        while(first <= last){                                 // n.log(n)
+            let mid = Math.floor((first + last)/2)
+            if(isvalid(arr, mid, k)){
+                ans = mid;
+                last = mid;
+            } else first = mid + 1
+        }
+        return ans;
+    }
+}
+
+// T.c -- O(n + n.log(n))  == n.log(n)
+// S.C -- O(1)
--- Advanced/Binary Search Algorithm.js
+++ Advanced/Binary Search Algorithm.js
@@ -2,8 +2,8 @@
 
 let arr = [2,5,8,9,12,18,20,25,67,101,125]
 let index = BinarySearch(arr, 0, arr.length-1, 67)
-else console.log("Target Fount at " + index + " index");
 if(index == -1) console.log("Not Found");
+else console.log("Target Found at " + index + " index");
 
 
 function BinarySearch(arr, first, last){
