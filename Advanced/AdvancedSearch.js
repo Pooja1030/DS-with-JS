47b42aabb785765ca32990db8599c7575ab01b93
Pooja <poojasawant1030@gmail.com>
2025-05-21 13:55:17 +0530

    done with Koko Eating Banana

--- Advanced/AdvancedSearch.js
+++ Advanced/AdvancedSearch.js
@@ -39,3 +39,39 @@ class Solution {
 
 // T.c -- O(n + n.log(n))  == n.log(n)
 // S.C -- O(1)
+
+
+
+// KOKO Eating Banana
+
+var isvalid = function(pile, speed, h){
+    let totalHour = 0;
+    for(let i = 0; i < pile.length(); i++){
+        if(pile[i] % speed == 0){
+            totalHour += pile[i] / speed;
+        } else totalHour += Math.ceil(pile[i] / speed);
+          if(totalHour > h)  return false;
+    }
+    return true;
+}
+
+var minEatingSpeed = function(arr, h){
+    let first = 1;
+    let last = 0;
+    let ans = -1;
+    for(let i = 0; i < arr.length; i++){
+        last = Math.max(arr[i], last);
+    }
+    while(first <= last){                                 // n.log(n)
+        let mid = Math.floor((first + last)/2)
+        if(isvalid(arr, mid, h)){
+            ans = mid;
+            last = mid - 1;
+        } else first = mid + 1
+    }
+    return ans;
+}
+
+
+
+// Threshold
